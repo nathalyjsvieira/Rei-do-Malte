2e651d1f7b2a30860a42817837e74a9414f42dc2
Nathaly Vieira <nathaly.vieiracs@gmail.com>
2025-05-25 22:39:24 -0300

    photos section finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
 }
 
 const swiperStages = new Swiper('.stages__swiper', {
@@ -23,4 +23,26 @@ const swiperStages = new Swiper('.stages__swiper', {
     prevEl: '.swiper-button-prev',
   },
 
+});
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });
